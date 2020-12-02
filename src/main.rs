c2bc6a9ce0cb892f8bf3a618271381020a3dff0d
Patrick White <patrick@patrickwhite.org>
2020-12-02 09:05:21 -0800

    day2: part1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,61 @@
-use std::io::{BufReader, stdin};
-use std::io::{Read, BufRead};
+use std::{str::FromStr, io::{BufReader, stdin}};
+use std::io::{BufRead};
 use std::collections::HashSet;
+use regex;
+
+#[derive(Debug, Eq, PartialEq)]
+struct Policy{ 
+    low: usize,
+    high: usize,
+    target: String,
+}
+
+impl FromStr for Policy {
+    type Err = ();
+
+    // example policy line:
+    // 3-7 r: mxvlzcjrsqst
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let rx = r"(\d+)-(\d+) ([[:alpha:]]+)";
+        let reg = regex::Regex::new(rx).map_err(|_| ())?;
+        let r = reg.captures(s).ok_or(())?;
+        let low = r[1].parse().unwrap();
+        let high = r[2].parse().unwrap();
+        let target: String = r[3].to_string();
+        Ok(Policy {
+            low,
+            high,
+            target,
+        })
+    }
+}
 
 fn main() {
-    let mut numbers: Vec<i32> = Default::default();
     let r = BufReader::new(stdin());
-    for line in r.lines() {
-        if let Ok(line) = line {
-            numbers.push(line.trim().parse().unwrap())
+
+    let mut valid = 0;
+    for line in r.lines().filter_map(Result::ok) {
+        let mut parts = line.split(':');
+        let policy: Policy = parts.next().unwrap().parse().unwrap();
+        let test_me = parts.next().unwrap().trim();
+
+        let how_many = test_me.matches(&policy.target).count();
+        if (policy.low..=policy.high).contains(&how_many) {
+            valid += 1;
         }
     }
-    for number1 in numbers.iter() {
-        let start = 2020 - number1;
-        for number in numbers.iter() {
-            let result = start - number;
-            if numbers.contains(&result) {
-                println!("{} {} {} = {}", number1, number, result,  number1 * number * result);
-                println!("{} {} {} | {} , {}", number1, number, result, number1 + number + result, number1 * number * result);
-            }
-        }
+
+    dbg!(valid);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_one() {
+        let test = "3-7 r";
+        let pol: Policy = test.parse().unwrap();
+        assert_eq!(pol, Policy{low: 3, high: 7, target: "r".to_string()});
     }
 }
