631ce5a76e57b56b1556067b6a698778f79b005f
Patrick White <patrick@patrickwhite.org>
2020-12-06 10:41:17 -0800

    day6 part 1

--- src/main.rs
+++ src/main.rs
@@ -8,72 +8,44 @@ use std::{
     str::FromStr,
 };
 
-const MAX_ROW: u8 = 128;
-const MAX_COL: u8 = 8;
-
-#[derive(Debug, Eq, PartialEq)]
-struct SeatLocation {
-    row: u8,
-    column: u8,
-    seat_id: u16,
-}
-
-impl SeatLocation {
-    fn new<T: AsRef<str>>(s: T) -> Self {
-        let s = s.as_ref();
-        assert_eq!(s.len(), 10);
-        let row = char_to_bin(&s[0..7], 'F', 'B');
-        let column = char_to_bin(&s[7..10], 'L', 'R');
-        SeatLocation {
-            row,
-            column,
-            seat_id: row as u16 * MAX_COL as u16 + column as u16,
+fn crunch_batch(batch: &mut dyn BufRead) -> Vec<u32> {
+    let mut out = Vec::new();
+    let mut lines = batch.lines();
+    let mut acc = 0;
+    while let Some(Ok(line)) = lines.next() {
+        if line.is_empty() {
+            out.push(acc);
+            acc = 0;
+        } else {
+            acc |= str_to_bin(line.trim());
         }
     }
-}
+    out.push(acc);
 
-impl std::cmp::Ord for SeatLocation {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.seat_id.cmp(&other.seat_id)
-    }
-}
-
-impl std::cmp::PartialOrd for SeatLocation {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.seat_id.partial_cmp(&other.seat_id)
-    }
-}
-
-
-fn char_to_bin(s: &str, low: char, high: char) -> u8 {
-    let mut out = 0;
-    for c in s.chars() {
-        // using the F/B example, if we have F, push a 0
-        // if we have B, push a 1
-        match c {
-            _ if c == low => out <<= 1,
-            _ if c == high => out = out << 1 | 1,
-            _ => panic!("found a string with strange runes"),
-        }
-    }
     out
 }
 
 fn main() {
-    let r = BufReader::new(stdin());
-    let mut seats: Vec<_> = r.lines()
-        .filter_map(Result::ok)
-        .map(SeatLocation::new)
-        .collect();
-    seats.sort_unstable();
+    let mut r = BufReader::new(stdin());
+    let answers = crunch_batch(&mut r);
 
-    for seats in (&seats).windows(2) {
-        let this = seats[0].seat_id;
-        let next = seats[1].seat_id;
-        if next - this == 2 {
-            println!("missing seat: {}", this + 1);
-        }
-    }
+    let yeses: u32 = answers
+        .iter()
+        .map(|i| i.count_ones())
+        .sum();
+    println!("total yes: {}", yeses);
+}
+
+fn char_to_bin(c: char) -> u32 {
+    c.to_digit(36).unwrap() - 10
+}
+
+fn str_to_bin(s: &str) -> u32 {
+    s.chars()
+        .map(char_to_bin)
+        .fold(0, |init, c| {
+            init | (1 << c)
+        })
 }
 
 #[cfg(test)]
@@ -81,46 +53,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_rows() {
-        let known = [
-            ("BFFFBBFRRR", 70),
-            ("FFFBBBFRRR", 14),
-            ("BBFFBBFRLL", 102),
-        ];
-        for (case, answer) in known.iter() {
-            let row = &case[0..7];
-            assert_eq!(char_to_bin(row, 'F', 'B'), *answer);
-        }
+    fn test_str_to_bin() {
+        assert_eq!(str_to_bin("abcd"), 0b1111);
+        assert_eq!(str_to_bin("ace"), 0b10101);
+        assert_eq!(str_to_bin("abz"), 0b10000000000000000000000011);
     }
 
     #[test]
-    fn test_cols() {
-        let known = [
-            ("BFFFBBFRRR", 7),
-            ("FFFBBBFRRR", 7),
-            ("BBFFBBFRLL", 4),
-        ];
-        for (case, answer) in known.iter() {
-            let row = &case[7..10];
-            assert_eq!(case.len(), 10);
-            assert_eq!(char_to_bin(row, 'L', 'R'), *answer);
-        }
+    fn test_char_to_bin() {
+        assert_eq!(char_to_bin('a'), 0);
+        assert_eq!(char_to_bin('z'), 25);
     }
 
     #[test]
-    fn test_locations() {
-        let known = [
-            ("BFFFBBFRRR", 70, 7, 567),
-            ("FFFBBBFRRR", 14, 7, 119),
-            ("BBFFBBFRLL", 102, 4, 820),
-        ];
-        for (case, row, column, seat_id) in known.iter() {
-            let rhs = SeatLocation {
-                row: *row,
-                column: *column,
-                seat_id: *seat_id,
-            };
-            assert_eq!(SeatLocation::new(case), rhs);
-        }
+    fn test_examples() {
+        let data = "abc
+
+                        a
+                        b
+                        c
+
+                        ab
+                        ac
+
+                        a
+                        a
+                        a
+                        a
+
+                        b\n";
+        let mut buf = std::io::Cursor::new(data); 
+        let results = crunch_batch(&mut buf);
+        assert_eq!(results, vec![0b111, 0b111, 0b111, 0b1, 0b10]);
     }
 }
